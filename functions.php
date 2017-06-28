8e1435d09b63b06e8fa7a4a39d8bbb1af0bdea6c
Jason Vinson <vinsonizer@gmail.com>
2017-06-27 21:25:49 -0400

    Initial Commit

---
+++ functions.php
@@ -0,0 +1,45 @@
+<?php
+
+// add bootstrap js to the header
+function scripts_load_bootstrap()
+{
+     
+	wp_register_script( 'bootstrap-js-cdn', 
+		'//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js',
+		array(), null, false );
+	wp_enqueue_script( 'bootstrap-js-cdn' );
+}
+add_action( 'wp_enqueue_scripts', 'scripts_load_bootstrap' );
+
+// add bootstrap and font-awesome to the header
+function styles_load_bootstrap()
+{
+	wp_register_style( 'font-awesome-style-cdn',
+		'//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/
+css/font-awesome.min.css',
+		array(), '4.7.0', 'all' );
+	wp_enqueue_style( 'font-awesome-style-cdn' );
+
+	wp_register_style( 'bootstrap-style-cdn',
+		'//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css',
+		array(), '3.3.7', 'all' );
+	wp_enqueue_style( 'bootstrap-style-cdn' );
+
+	wp_register_style( 'bootstrap-theme-style-cdn',
+		'//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css',
+		array('bootstrap-style-cdn'), '3.3.7', 'all' );
+	wp_enqueue_style( 'bootstrap-theme-style-cdn' );
+
+}
+add_action( 'wp_enqueue_scripts', 'styles_load_bootstrap' );
+
+// Filter the wp_nav_menu() to add your new menu item
+function add_nav_menu_items($items) {
+    $twitterlink= '<li class="right-nav"><a href="https://www.twitter.com/F3thefort"><i class="fa fa-twitter"></i></a></li>';
+    $slacklink= '<li class="right-nav"><a href="https://f3thefort.slack.com/"><i class="fa fa-slack"></i></a></li>';
+    $items = $items . $twitterlink . $slacklink;
+    return $items;
+}
+add_filter( 'wp_nav_menu_items', 'add_nav_menu_items', 10, 2 );
+
+?>
