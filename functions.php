61af9c75e1b681818ce93f91f0b59fb91f53acbe
Jason Vinson <vinsonizer@gmail.com>
2017-06-28 09:10:07 -0400

    cleaned up whitespace

--- functions.php
+++ functions.php
@@ -4,35 +4,35 @@
 function scripts_load_bootstrap()
 {
      
-	wp_register_script( 'bootstrap-js-cdn', 
-		'//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js',
-		array('jquery'), null, false );
-	wp_enqueue_script( 'bootstrap-js-cdn' );
+  wp_register_script( 'bootstrap-js-cdn', 
+    '//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js',
+    array('jquery'), null, false );
+  wp_enqueue_script( 'bootstrap-js-cdn' );
 }
 add_action( 'wp_enqueue_scripts', 'scripts_load_bootstrap' );
 
 // add bootstrap and font-awesome to the header
 function styles_load_custom()
 {
-	wp_register_style( 'google-fonts-roboto-style',
+  wp_register_style( 'google-fonts-roboto-style',
     '//fonts.googleapis.com/css?family=Roboto',
-		array(), '', 'all' );
-	wp_enqueue_style( 'google-fonts-roboto-style' );
+    array(), '', 'all' );
+  wp_enqueue_style( 'google-fonts-roboto-style' );
 
-	wp_register_style( 'font-awesome-style-cdn',
-		'//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css',
-		array(), '4.7.0', 'all' );
-	wp_enqueue_style( 'font-awesome-style-cdn' );
+  wp_register_style( 'font-awesome-style-cdn',
+    '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css',
+    array(), '4.7.0', 'all' );
+  wp_enqueue_style( 'font-awesome-style-cdn' );
 
-	wp_register_style( 'bootstrap-style-cdn',
-		'//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css',
-		array(), '3.3.7', 'all' );
-	wp_enqueue_style( 'bootstrap-style-cdn' );
+  wp_register_style( 'bootstrap-style-cdn',
+    '//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css',
+    array(), '3.3.7', 'all' );
+  wp_enqueue_style( 'bootstrap-style-cdn' );
 
-	wp_register_style( 'bootstrap-theme-style-cdn',
-		'//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css',
-		array('bootstrap-style-cdn'), '3.3.7', 'all' );
-	wp_enqueue_style( 'bootstrap-theme-style-cdn' );
+  wp_register_style( 'bootstrap-theme-style-cdn',
+    '//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css',
+    array('bootstrap-style-cdn'), '3.3.7', 'all' );
+  wp_enqueue_style( 'bootstrap-theme-style-cdn' );
 
 }
 add_action( 'wp_enqueue_scripts', 'styles_load_custom' );
