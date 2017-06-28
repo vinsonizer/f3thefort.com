d58c75c2b4326d1226d0289bc3f915b4ef084e75
Jason Vinson <vinsonizer@gmail.com>
2017-06-28 08:52:26 -0400

    Added Roboto Google Font as default

--- functions.php
+++ functions.php
@@ -12,11 +12,15 @@ function scripts_load_bootstrap()
 add_action( 'wp_enqueue_scripts', 'scripts_load_bootstrap' );
 
 // add bootstrap and font-awesome to the header
-function styles_load_bootstrap()
+function styles_load_custom()
 {
+	wp_register_style( 'google-fonts-roboto-style',
+    '//fonts.googleapis.com/css?family=Roboto',
+		array(), '', 'all' );
+	wp_enqueue_style( 'google-fonts-roboto-style' );
+
 	wp_register_style( 'font-awesome-style-cdn',
-		'//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/
-css/font-awesome.min.css',
+		'//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css',
 		array(), '4.7.0', 'all' );
 	wp_enqueue_style( 'font-awesome-style-cdn' );
 
@@ -31,7 +35,7 @@ css/font-awesome.min.css',
 	wp_enqueue_style( 'bootstrap-theme-style-cdn' );
 
 }
-add_action( 'wp_enqueue_scripts', 'styles_load_bootstrap' );
+add_action( 'wp_enqueue_scripts', 'styles_load_custom' );
 
 // Filter the wp_nav_menu() to add your new menu item
 function add_nav_menu_items($items) {
